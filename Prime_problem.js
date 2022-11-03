24d64e422599a8329f9fee33e7fe5e0d6771e437
ankit <ankraz123@gmail.com>
2022-11-03 14:03:32 +0530

    check, it is right or not

---
+++ Prime_problem.js
@@ -0,0 +1,15 @@
+let n=11;
+
+for(j=1; j<=n; j++){
+  
+count;
+for(i=1; i<=N; i++){
+    if(j%i==0){
+      
+    }
+}if(count==2){
+    console.log(j,"it is prime");
+}else{
+    console.log(j,"it is not prime");
+}
+}
